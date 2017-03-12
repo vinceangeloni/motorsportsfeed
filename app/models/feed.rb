e4069e37f01db99fe1bea6ba39839b94228a836c
Vince Angeloni <vangeloni@activecampaign.com>
2017-03-12 12:54:12 -0400

    Pass to Superfeedr

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,8 +1,10 @@
 class Feed < ActiveRecord::Base
 	before_save :generate_unique_token
+	after_save :send_to_superfeedr
 	has_many :entries, dependent: :destroy
 	validates :url, uniqueness: true
 	
+
 	def generate_unique_token(length=50)
 	    loop do
       		secret = self.secret = SecureRandom.urlsafe_base64(length, false)
@@ -11,11 +13,16 @@ class Feed < ActiveRecord::Base
 	end
 
 	def notified params
-    update_attributes(:status => params["status"]["http"])
+	    update_attributes(:status => params["status"]["http"])
 
-  	params['items'].each do |i|
-  		entries.create(:title => i["title"], :url => i["permalinkUrl"], :content => i["content"])
+	  	params['items'].each do |i|
+	  		entries.create(:title => i["title"], :url => i["permalinkUrl"], :content => i["content"])
+	  	end
   	end
-  end
+
+  	private
+	  	def send_to_superfeedr 
+	  		SuperfeedrEngine::Engine.subscribe(self, {:retrieve => true})
+	  	end
 
 end
